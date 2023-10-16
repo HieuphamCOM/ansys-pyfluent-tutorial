8decc87e75e610700cdc973f33f44d19e8ea9c76
ALEXPHAM\hieup <hieupham.auttech@gmail.com>
2023-10-16 23:18:45 +0700

    [ADD] MAIN: Add files for lesson 3,

---
+++ test.py
@@ -0,0 +1,7 @@
+try:
+    import ansys.fluent.core as pyfluent
+    flglobals = pyfluent.setup_for_fluent(product_version="23.2.0", mode="meshing", version="3d", precision="double", processor_count=1)
+    globals().update(flglobals)
+except Exception:
+    pass
+
